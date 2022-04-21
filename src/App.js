a12fbde2d0557a29e4a11400b1d4e4164bc771b2
satyadav123 <sk259454@gmail.com>
2022-04-21 08:44:03 +0530

    counter-color-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [counter,setCounter]=useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1  className={`${(counter%2===0 )? "even":"odd"}`}>COUNTER:{counter}</h1>
+      <button onClick={()=>{
+       setCounter(counter+1)
+       console.log("clicked")
+     }}>ADD</button>
+      <button
+      onClick={()=>{
+        setCounter(counter-1)
+        console.log("clicked")
+      }}>SUB</button>
+    </div>
+   <button  onClick={()=>{
+        setCounter(counter*2)
+       
+      }}>double</button> 
     </div>
   );
 }
